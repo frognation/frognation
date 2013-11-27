388c5a0684769e3fbf567843fd65929433f27ab0
frognation <frognation7@gmail.com>
2013-11-28 02:32:34 +0900

    this
    gurae gurae

---
+++ index_files/main.js
@@ -0,0 +1,16 @@
+var scene;
+
+var init = function() {
+    scene = document.querySelector('#scene');
+    
+};
+
+window.onload = function() {
+    init();
+};
+
+window.onmousemove = function(e) {
+    var dx = window.innerWidth / 2 - e.pageX;
+    var s = 'rotateY(' + dx + 'deg)';
+    scene.style['-webkit-transform'] = s;
+};
